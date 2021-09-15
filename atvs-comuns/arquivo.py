0fb380713ffd687f7fd06f770d64844b874a3b72
Edu-tentando-49 <pudimcomcaramelo49@gmail.com>
2021-09-15 20:36:25 -0300

    atualizei diagrama e anotei mais um tanto

---
+++ atvs-comuns/arquivo.py
@@ -0,0 +1,9 @@
+# deve ter algo para mesa, que deve ter ocupação;
+# deve ter algo para cliente;
+# deve ter algo para pedido, armazenando valor na conta;
+# deve ter algo para prato, com valor e nome;
+# para cada conta(comanda), somente deve estar conectado um identificador;
+# algo deve contar a quantidade de mesas disponíveis(vagas);
+# algo deve detectar quando o pagamento está acima da conta, para que seja feito o troco;
+#
+#
